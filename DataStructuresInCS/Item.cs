c221bcdde009cee7de45c1fdc19972303a680acf
Sarah <83969766+sgeorge22@users.noreply.github.com>
2021-06-10 15:07:35 -0400

    Add project files.

---
+++ DataStructuresInCS/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresInCSLib
+{
+    //this class will contain the data the user wants to store
+    public class Item<T>//adding the <T> acts as a data type, in this case T = int
+    {
+        public T t { get; set; }
+        public int Next { get; set; } = -1; //next and prev are pointing to the array to keep things in order
+        public int Prev { get; set; } = -1;
+
+        public Item(T t)
+        {
+            this.t = t;
+        }
+
+
+
+    }
+}
---
+++ DataStructuresInCSConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DataStructuresInCSLib;
+
+
+namespace DataStructuresInCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<int>();
+            for(var idx = 0; idx < 100; idx++)
+            {
+                stack.Push(idx);
+            }
+            while (!stack.Empty)
+            {
+                Console.WriteLine($"{stack.Pop()}");
+            }
+            
+        }
+    }
+}
